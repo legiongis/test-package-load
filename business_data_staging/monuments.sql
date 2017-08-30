dbefc6143220cad2e60f4656349db370999a4a08
Cyrus Hiatt <chiatt@fargeo.com>
2017-08-30 09:49:12 -0700

    Added comments, converted primary name valueid to its label.

--- business_data_staging/monuments.sql
+++ business_data_staging/monuments.sql
@@ -1,14 +1,14 @@
 CREATE OR REPLACE VIEW vw_monuments AS
 with mv as (select tileid, resourceinstanceid, nodeid, ST_Union(geom) as geom, ST_GeometryType(geom) as geom_type, count(tileid) as count
 	from mv_geojson_geoms
-	group by tileid, nodeid, resourceinstanceid, ST_GeometryType(geom))
+	group by tileid, nodeid, resourceinstanceid, ST_GeometryType(geom)) -- Union like geometry types within the same tile into a multipart geometry
 select row_number() over () as gid,
 	mv.tileid,
 	mv.nodeid,
 	ST_GeometryType(geom) as geom_type,
 	name_tile.tiledata ->> '677f303d-09cc-11e7-9aa6-6c4008b05c4c' as name,
 	name_tile.tiledata ->> '677f39a8-09cc-11e7-834a-6c4008b05c4c' as nametype,
-	(select value from values where cast(component.tiledata ->>'ab74b009-fa0e-11e6-9e3e-026d961c88e6' as uuid) = valueid ) as cot,
+	(select value from values where cast(component.tiledata ->>'ab74b009-fa0e-11e6-9e3e-026d961c88e6' as uuid) = valueid ) as construction_type,
 	component.tiledata ->> 'ab74afec-fa0e-11e6-9e3e-026d961c88e6' as const_tech,
 	(select value from values where cast(record.tiledata ->> '677f2c0f-09cc-11e7-b412-6c4008b05c4c' as uuid) = valueid ) as record_type,
 	geom
