6f6658f1b07b027436ae8613d25994cd6a1da5de
Cyrus Hiatt <chiatt@fargeo.com>
2017-08-29 17:54:12 -0700

    Restricting results to the monument resource model and converting the record type id to a label.

--- business_data_staging/monuments.sql
+++ business_data_staging/monuments.sql
@@ -4,7 +4,7 @@ select row_number() over () as gid,
 	name_tile.tiledata ->> '677f39a8-09cc-11e7-834a-6c4008b05c4c' as nametype,
 	(select value from values where cast(component.tiledata ->>'ab74b009-fa0e-11e6-9e3e-026d961c88e6' as uuid) = valueid ) as cot,
 	component.tiledata ->> 'ab74afec-fa0e-11e6-9e3e-026d961c88e6' as const_tech,
-	record.tiledata ->> '677f2c0f-09cc-11e7-b412-6c4008b05c4c' as record_type
+	(select value from values where cast(record.tiledata ->> '677f2c0f-09cc-11e7-b412-6c4008b05c4c' as uuid) = valueid ) as record_type
 from mv_geojson_geoms mv
 left join tiles name_tile
 	 on mv.resourceinstanceid = name_tile.resourceinstanceid
@@ -18,3 +18,4 @@ left join tiles record
         on name_tile.resourceinstanceid = record.resourceinstanceid
         and record.tiledata->>'677f2c0f-09cc-11e7-b412-6c4008b05c4c'
 		!= ''
+where (select graphid from resource_instances where mv.resourceinstanceid = resourceinstanceid) = 'ab74af76-fa0e-11e6-9e3e-026d961c88e6'
