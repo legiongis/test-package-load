458a430575ca3b65bcabee784b45e16d22ff833d
Cyrus Hiatt <chiatt@fargeo.com>
2017-08-30 09:39:21 -0700

    Union like geometry types within the same tile as multipart.

--- business_data_staging/monuments.sql
+++ business_data_staging/monuments.sql
@@ -1,11 +1,17 @@
+with mv as (select tileid, resourceinstanceid, nodeid, ST_Union(geom) as geom, ST_GeometryType(geom) as geom_type, count(tileid) as count
+	from mv_geojson_geoms
+	group by tileid, nodeid, resourceinstanceid, ST_GeometryType(geom))
 select row_number() over () as gid,
-	mv.*,
+	mv.tileid,
+	mv.nodeid,
+	ST_GeometryType(geom) as geom_type,
 	name_tile.tiledata ->> '677f303d-09cc-11e7-9aa6-6c4008b05c4c' as name,
 	name_tile.tiledata ->> '677f39a8-09cc-11e7-834a-6c4008b05c4c' as nametype,
 	(select value from values where cast(component.tiledata ->>'ab74b009-fa0e-11e6-9e3e-026d961c88e6' as uuid) = valueid ) as cot,
 	component.tiledata ->> 'ab74afec-fa0e-11e6-9e3e-026d961c88e6' as const_tech,
-	(select value from values where cast(record.tiledata ->> '677f2c0f-09cc-11e7-b412-6c4008b05c4c' as uuid) = valueid ) as record_type
-from mv_geojson_geoms mv
+	(select value from values where cast(record.tiledata ->> '677f2c0f-09cc-11e7-b412-6c4008b05c4c' as uuid) = valueid ) as record_type,
+	geom
+from mv
 left join tiles name_tile
 	 on mv.resourceinstanceid = name_tile.resourceinstanceid
 	 and name_tile.tiledata->>'677f39a8-09cc-11e7-834a-6c4008b05c4c'
