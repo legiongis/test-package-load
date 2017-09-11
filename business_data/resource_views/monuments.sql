d2dacdead9d19cd2a110aa283e3d2d7048c25b22
Cyrus Hiatt <chiatt@fargeo.com>
2017-09-11 14:27:17 -0700

    Updated shapefile export view to convert concept list values to labels.

--- business_data/resource_views/monuments.sql
+++ business_data/resource_views/monuments.sql
@@ -4,13 +4,14 @@ with mv as (select tileid, resourceinstanceid, nodeid, ST_Union(geom) as geom, S
 	from mv_geojson_geoms
 	group by tileid, nodeid, resourceinstanceid, ST_GeometryType(geom)) -- Union like geometry types within the same tile into a multipart geometry
 select row_number() over () as gid,
+        mv.resourceinstanceid,
 	mv.tileid,
 	mv.nodeid,
 	ST_GeometryType(geom) as geom_type,
-	name_tile.tiledata ->> '677f303d-09cc-11e7-9aa6-6c4008b05c4c' as name,
-	(select value from values where cast(name_tile.tiledata ->> '677f39a8-09cc-11e7-834a-6c4008b05c4c' as uuid) = valueid ) as nametype,
+	name_tile.tiledata ->> '677f303d-09cc-11e7-9aa6-6c4008b05c4c' as name,  -- get a simple string value from tile json
+	(select value from values where cast(name_tile.tiledata ->> '677f39a8-09cc-11e7-834a-6c4008b05c4c' as uuid) = valueid ) as nametype, -- get a concept's value label
 	(select value from values where cast(component.tiledata ->>'ab74b009-fa0e-11e6-9e3e-026d961c88e6' as uuid) = valueid ) as construction_type,
-	component.tiledata ->> 'ab74afec-fa0e-11e6-9e3e-026d961c88e6' as const_tech,
+	array_to_string((select array_agg(v.value) from unnest(ARRAY(SELECT jsonb_array_elements_text(component.tiledata -> 'ab74afec-fa0e-11e6-9e3e-026d961c88e6'))::uuid[]) item_id left join values v on v.valueid=item_id), ',') as const_tech, -- get the value labels from a concept list
 	(select value from values where cast(record.tiledata ->> '677f2c0f-09cc-11e7-b412-6c4008b05c4c' as uuid) = valueid ) as record_type,
 	geom
 from mv
